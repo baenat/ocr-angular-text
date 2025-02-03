a41b05331e605717a5b31eb71a66794eb043e77c
baenat <bae.julian10@gmail.com>
2025-02-03 10:27:35 -0500

    Reorder imports in components.module.ts for better organization

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { WebcamModule } from 'ngx-webcam';
 import { CameraComponent } from './camera/camera.component';
 import { HeaderComponent } from './header/header.component';
-import { TesseractComponent } from './tesseract/tesseract.component';
 import { ImageFileComponent } from './image-file/image-file.component';
+import { TesseractComponent } from './tesseract/tesseract.component';
 
 @NgModule({
   declarations: [
